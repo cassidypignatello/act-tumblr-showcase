993f8dfb47dc3e54a0b248a552f61f5b34379a22
Cassidy Pignatello <cassidy.pignatello@gmail.com>
2015-08-05 15:30:58 -0400

    adds styling for filtered elements to sidebar

--- js/isotope-combo-filter.js
+++ js/isotope-combo-filter.js
@@ -14,17 +14,16 @@ $(function(){
   $('.filter li a').click(function() {
     var $this = $(this);
 
-    if ($this.hasClass('selected')) {
-      return false;
-    }
+    $('#vertical-all').removeClass('selected');
     
     var $optionSet = $this.parents('.option-set');
-    $optionSet.find('.selected').removeClass('selected');
     $this.addClass('selected');
 
     // get each filter value and add it to the array
     $filterValue = $this.attr('data-filter-value');
     if ($filterValue == "*") {
+      $optionSet.find('.selected').removeClass('selected');
+      $this.addClass('selected');
       isoFilters = [];
     } else {
       isoFilters.push($filterValue);
